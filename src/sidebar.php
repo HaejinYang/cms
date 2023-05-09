b6cc4a5a6d27dbeb827067d25be005fabed7b62d
haejin <haejin.yang91@gmail.com>
2023-05-10 04:00:59 +0900

    feat: 사이드바 카테고리를 연동

--- src/sidebar.php
+++ src/sidebar.php
@@ -19,11 +19,11 @@
     <div class="well">
         <h4>Blog Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
                     <?php
                     require_once $_SERVER['DOCUMENT_ROOT'].'/db/db.php';
-                    $result = DB::query("SELECT * FROM category");
+                    $result = DB::query("SELECT * FROM category LIMIT 3");
 
                     $el = '';
                     while($row = $result->fetch_assoc()) {
@@ -35,20 +35,6 @@
                     ?>
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <!--                        <div class="col-lg-6">-->
-            <!--                            <ul class="list-unstyled">-->
-            <!--                                <li><a href="#">Category Name</a>-->
-            <!--                                </li>-->
-            <!--                                <li><a href="#">Category Name</a>-->
-            <!--                                </li>-->
-            <!--                                <li><a href="#">Category Name</a>-->
-            <!--                                </li>-->
-            <!--                                <li><a href="#">Category Name</a>-->
-            <!--                                </li>-->
-            <!--                            </ul>-->
-            <!--                        </div>-->
-            <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
     </div>
