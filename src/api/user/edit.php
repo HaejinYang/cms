1bd1cae8b947c8eb4b03a103b5f706a7f158fb01
haejin <haejin.yang91@gmail.com>
2023-05-25 22:48:55 +0900

    feat: 프로필 업데이트를 위해 기존 user/edit을 수정

--- src/api/user/edit.php
+++ src/api/user/edit.php
@@ -11,13 +11,31 @@ do {
         break;
     }
 
-    $user_dao = new UserStore();
-    $result = $user_dao->update($_POST['id'], $_POST['account'], $_POST['password'], $_POST['password_check'], $_POST['lastname'],
-        $_POST['firstname'], $_POST['email'], $_POST['role']);
-    if ($result !== UserStore::ERROR_OK) {
-        $response_msg = UserStore::getErrorCodeToMsg($result);
+    try {
+        $user_dao = new UserStore();
+        $result = $user_dao->read($user, $_POST['id']);
+        if ($result !== UserStore::ERROR_OK) {
+            $response_msg = UserStore::getErrorCodeToMsg($result);
 
-        break;
+            break;
+        }
+
+        $password = $_POST['password'];
+        $password_check = $_POST['password_check'];
+        if (empty($password) && empty($password_check)) {
+            $password = $user['password'];
+            $password_check = $password;
+        }
+
+        $result = $user_dao->update($_POST['id'], $_POST['account'], $password, $password_check, $_POST['lastname'],
+            $_POST['firstname'], $_POST['email'], $_POST['role']);
+        if ($result !== UserStore::ERROR_OK) {
+            $response_msg = UserStore::getErrorCodeToMsg($result);
+
+            break;
+        }
+    } catch (mysqli_sql_exception $e) {
+        $response_msg = "잘못된 요청입니다.";
     }
 
     $is_success = true;
